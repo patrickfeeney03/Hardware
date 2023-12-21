f0dcafc2c2f7b275086b0b8387f8b27de67e62f0
Patrick <g00409592@atu.ie>
2023-12-21 13:07:48 +0000

    Centralize cpu get mapping

--- src/main/java/ie/atu/HardwareController.java
+++ src/main/java/ie/atu/HardwareController.java
@@ -13,10 +13,31 @@ public class HardwareController {
         this.hardwareService = hardwareService;
     }
 
+    /*
     @GetMapping("/getCPUByBrand")
-    public ResponseEntity<?> getCPU(@RequestParam("brand") String brand) {
+    public ResponseEntity<?> getCPUByBrand(@RequestParam("brand") String brand) {
         return ResponseEntity.ok(hardwareService.getCPUByBrand(brand));
     }
+
+    @GetMapping("cpu")
+    public ResponseEntity<?> getCPUByName(@RequestParam("name") String name) {
+        return ResponseEntity.ok(hardwareService.getCPUByName(name));
+    }
+     */
+
+    @GetMapping("/cpus")
+    public ResponseEntity<?> getCPUs(@RequestParam(name = "brand", required = false) String brand,
+                                     @RequestParam(name = "name", required = false) String name) {
+        if (brand != null) {
+            return ResponseEntity.ok(hardwareService.getCPUByBrand(brand));
+        } else if (name != null) {
+            return ResponseEntity.ok(hardwareService.getCPUByName(name));
+        } else {
+            return ResponseEntity.ok("No parameters provided");
+        }
+    }
+
+    // @GetMapping("/getCPUByName") PRICE
 
     @PostMapping("/createCPU")
     public ResponseEntity<String> createCPU(@RequestBody CPU cpu) {
