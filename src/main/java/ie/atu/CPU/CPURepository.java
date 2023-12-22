195e812ca6dfd62eb13a62209b5f1d70b63b2b71
Patrick <g00409592@atu.ie>
2023-12-22 22:20:46 +0000

    Remove unnecessary code

--- src/main/java/ie/atu/CPU/CPURepository.java
+++ src/main/java/ie/atu/CPU/CPURepository.java
@@ -6,11 +6,4 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 
 public interface CPURepository extends JpaRepository<CPU, Long>, JpaSpecificationExecutor<CPU> {
-    // This seems to be If I want to create special queries
-
-    List<CPU> findByBrand(String brand);
-
-    List<CPU> findByName(String name);
-
-    List<CPU> findByPrice(Float price);
 }
